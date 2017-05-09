1348333cc741e43419845c2f79c8738581910d86
JOREN\Joren <joren.vermeir@hotmail.com>
2017-05-09 11:47:09 +0200

    Doener: Joren Denker: Thibault Class: DomainException Story: 2 Aanpassing

--- GamesuiteHangman/src/model/DomainException.java
+++ GamesuiteHangman/src/model/DomainException.java
@@ -6,7 +6,7 @@ public class DomainException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public DomainException(String message, Exception exception){
-		super(message, exception);
+	public DomainException(String message){
+		super(message);
 	}
 }
