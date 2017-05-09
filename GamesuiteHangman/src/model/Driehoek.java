314718f97788fe58ea670e2a3bf6e4012b802feb
AHermans <annelies.hermans@student.ucll.be>
2017-05-09 13:48:35 +0200

    denker : Dylan Teugels doener: Annelies Hermans user story: driehoek class: driehoek + driehoektest af

--- GamesuiteHangman/src/model/Driehoek.java
+++ GamesuiteHangman/src/model/Driehoek.java
@@ -8,11 +8,10 @@ public class Driehoek {
 	
 	public Driehoek(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3){
 		setHoekPunten(hoekPunt1, hoekPunt2, hoekPunt3);
-		
-		
+
 	}
 	
-	private void setHoekPunten(Punt hoekPunt1,Punt hoekPunt2,Punt hoekPunt3){
+	public void setHoekPunten(Punt hoekPunt1,Punt hoekPunt2,Punt hoekPunt3){
 		if(hoekPunt1 == null || hoekPunt2 == null || hoekPunt3 == null){
 			throw new DomainException("een van de punten is leeg");
 		}
@@ -34,4 +33,20 @@ public class Driehoek {
 		return hoekPunt3;
 	}
 	
+	@Override
+	public boolean equals(Object o){
+		boolean gelijk= false;
+		if( o instanceof Driehoek){
+			Driehoek h = (Driehoek)o;
+			if(super.equals(h) && this.hoekPunt1 == h.getHoekPunt1() && this.hoekPunt2 == h.getHoekPunt2() && this.hoekPunt3 == h.getHoekPunt3()){
+				gelijk = true;
+			}
+		}
+		return gelijk;
+	
+	}
+	
+	public String toString(){
+		return "Driehoek: hoekpunt1" + getHoekPunt1() + " - hoekpunt2" + getHoekPunt2() + " + hoekpunt3" + getHoekPunt3();
+	}
 }
