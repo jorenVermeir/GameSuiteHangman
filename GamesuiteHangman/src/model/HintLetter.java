423b8eff70428828c9b89d4bfd209255ba50a273
Thibault Stroobants <thibault0115@gmail.com>
2017-05-11 15:31:19 +0200

    Doener: Thibault Stroobants; Denker: Dylan Teugels; Story 12; Klasses HintLetter, HintWoord, AllTests en HangManUi zijn toegevoegd / volledig af

--- GamesuiteHangman/src/model/HintLetter.java
+++ GamesuiteHangman/src/model/HintLetter.java
@@ -2,24 +2,44 @@ package model;
 
 public class HintLetter {
 	
+	private char letter;
+	private boolean geraden;
+	
 	public HintLetter(char letter) {
-		
+		String strLetter = letter + "";
+		strLetter = strLetter.toLowerCase();
+		letter = strLetter.charAt(0);
+		this.letter = letter;
+		this.geraden = false;
 	}
 	
 	public boolean raad(char letter) {
+		if (this.geraden) {
+			return false;
+		}
+		String strLetter = letter + "";
+		strLetter = strLetter.toLowerCase();
+		letter = strLetter.charAt(0);
+		if (this.letter == letter) {
+			this.geraden = true;
+			return true;
+		}
 		return false;
 	}
 	
 	public boolean isGeraden() {
-		return false;
+		return this.geraden;
 	}
 	
 	public char toChar() {
-		return 'a';
+		if (this.geraden) {
+			return this.letter;
+		}
+		return '_';
 	}
 	
 	public char getLetter() {
-		return 'a';
+		return this.letter;
 	}
 
 }
--- GamesuiteHangman/src/model/HintWoord.java
+++ GamesuiteHangman/src/model/HintWoord.java
@@ -4,27 +4,53 @@ import java.util.ArrayList;
 
 public class HintWoord {
 	
-	ArrayList<HintLetter> hintLetters = new ArrayList<>();
+	private ArrayList<HintLetter> hintLetters = new ArrayList<>();
 	
 	public HintWoord(String woord) {
-		
+		if (woord == null || woord.trim().isEmpty()) {
+			throw new DomainException("Woord is leeg.");
+		}
+		for (char letter : woord.toCharArray()) {
+			hintLetters.add(new HintLetter(letter));
+		}
 	}
 	
 	public boolean raad(char letter) {
-		return false;
+		boolean geraden = false;
+		for (HintLetter hintLetter : hintLetters) {
+			if (hintLetter.raad(letter)) {
+				geraden = true;
+			}
+		}
+		return geraden;
 	}
 	
 	public boolean isGeraden() {
-		return false;
+		String geradenWoord = this.toString();
+		for (char letter : geradenWoord.toCharArray()) {
+			if (letter == '_') {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "";
+		String out = "";
+		for (HintLetter hintLetter : hintLetters) {
+			out += " " + hintLetter.toChar();
+		}
+		out = out.substring(1, out.length());
+		return out;
 	}
 	
 	public String getWoord() {
-		return "";
+		String out = "";
+		for (HintLetter hintLetter : hintLetters) {
+			out += hintLetter.getLetter();
+		}
+		return out;
 	}
 
 }
