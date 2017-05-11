18febbc0f2e19aa24de1e9cc9c4df883e00ee6ec
JOREN\Joren <joren.vermeir@hotmail.com>
2017-05-11 13:39:19 +0200

    UiException toegevoegd door Joren en Annelies, JOEPIEEEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ GamesuiteHangman/src/ui/UiException.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class UiException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UiException(String message){
+		super(message);
+	}
+}
