3c206cfc57ce4be865abfe2cfe4d59e092a3b4ba
Thibault Stroobants <thibault0115@gmail.com>
2017-05-11 13:49:54 +0200

    Lege methodes in HintLetter en HintWoord toegevoegd

--- GamesuiteHangman/src/model/HintLetter.java
+++ GamesuiteHangman/src/model/HintLetter.java
@@ -1,5 +1,25 @@
 package model;
 
 public class HintLetter {
+	
+	public HintLetter(char letter) {
+		
+	}
+	
+	public boolean raad(char letter) {
+		return false;
+	}
+	
+	public boolean isGeraden() {
+		return false;
+	}
+	
+	public char toChar() {
+		return 'a';
+	}
+	
+	public char getLetter() {
+		return 'a';
+	}
 
 }
--- GamesuiteHangman/src/model/HintWoord.java
+++ GamesuiteHangman/src/model/HintWoord.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+
 public class HintWoord {
+	
+	ArrayList<HintLetter> hintLetters = new ArrayList<>();
+	
+	public HintWoord(String woord) {
+		
+	}
+	
+	public boolean raad(char letter) {
+		return false;
+	}
+	
+	public boolean isGeraden() {
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	public String getWoord() {
+		return "";
+	}
 
 }
